bac30ba4d569abe93b197eb1ea8ff991c511a320
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-05-16 10:45:57 +0100

    Removed hard-coded files from template, moved to propagator.

--- cutnrun/analysis/H3K4me3/propagate.sh
+++ cutnrun/analysis/H3K4me3/propagate.sh
@@ -7,6 +7,7 @@ do
     newfile=de_${w}.Rmd 
     cat ../template_de.Rmd | sed "s/#_WIDTH_#/${w}/g" \
         | sed "s/#_MARK_#/H3K4me3/g" \
+        | sed 's/#_FILES_#/"SLX-15763.iPCRtagT001.CC6ULANXX.s_7.r.bam",\n    "SLX-15763.iPCRtagT006.CC6ULANXX.s_7.r.bam",\n    "SLX-15763.iPCRtagT011.CC6ULANXX.s_7.r.bam",\n    "SLX-15763.iPCRtagT034.CC6ULANXX.s_7.r.bam",\n    "SLX-15764.D708_D501.CC6ULANXX.s_8.r.bam",\n    "SLX-15764.D709_D502.CC6ULANXX.s_8.r.bam",\n    "SLX-15764.D710_D503.CC6ULANXX.s_8.r.bam",\n    "SLX-15764.D711_D504.CC6ULANXX.s_8.r.bam"/' \
         > ${newfile}
     echo "rmarkdown::render('${newfile}')" | R --no-save --slave
 done
