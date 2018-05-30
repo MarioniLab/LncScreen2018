d50abde28827e619195c8be78ab5924b06c8b458
LTLA <aaron.lun@cruk.cam.ac.uk>
2018-05-30 10:48:36 +0100

    Fixed bugs in replicate merging script.

--- chartseq/merge_reps.sh
+++ chartseq/merge_reps.sh
@@ -1,12 +1,18 @@
+#!/bin/bash
+#SBATCH -e merge.log
+#SBATCH -o merge.out
+#SBATCH -n 1
+#SBATCH --mem 2000
+
 cd fastq/
 allprefix=$(ls | grep "fq.gz$" | sed "s/.s_[0-9].r_[12].fq.gz$//g" | sort | uniq)
-for thing in ${allfiles}
+for thing in ${allprefix}
 do 
-    curfiles1=$(ls | grep "_1\\.fq.gz$" | grep "${allprefix}")
-    curfiles2=$(ls | grep "_2\\.fq.gz$" | grep "${allprefix}")
-    cat ${curfiles1} > ${allprefix}_1.fq.gz
+    curfiles1=$(ls | grep "_1\\.fq.gz$" | grep "${thing}")
+    curfiles2=$(ls | grep "_2\\.fq.gz$" | grep "${thing}")
+    cat ${curfiles1} > ${thing}_1.fq.gz
     rm ${curfiles1}
-    cat ${curfiles2} > ${allprefix}_2.fq.gz
+    cat ${curfiles2} > ${thing}_2.fq.gz
     rm ${curfiles2}
 done
 
