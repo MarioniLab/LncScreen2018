177e24502360fb11b870b7e42325256c9678cebc
Aaron Lun <alun@wehi.edu.au>
2018-09-18 15:12:38 +0100

    Added script to plot lnc expression from GTEx data.

---
+++ miscellaneous/gtex_plot.R
@@ -0,0 +1,22 @@
+# Loads in GTEx TPMs for the specified gene, and plots them by tissue.
+# Data taken from https://gtexportal.org/home/datasets
+
+library(readr)
+exprs <- read_tsv("GTEx_Analysis_2016-01-15_v7_RNASeQCv1.1.8_gene_tpm.gct", skip=2)
+meta <- read.table("GTEx_v7_Annotations_SampleAttributesDS.txt", header=TRUE, stringsAsFactors=FALSE, sep="\t")
+
+genes <- c(LINC00899="ENSG00000231711", C1qTNF1AS1="ENSG00000265096")
+for (x in seq_along(genes)) {
+    keep <- grep(genes[x], exprs$Name)
+    lnc.exprs <- as.numeric(exprs[keep,-(1:2)])
+
+    locale <- meta$SMTS[match(colnames(exprs)[-(1:2)], meta$SAMPID)]
+    by.location <- split(lnc.exprs, locale)
+    by.location <- by.location[names(by.location)!=""]
+    by.location <- by.location[order(-sapply(by.location, median))]
+
+    pdf(paste0(genes[x], "_GTEx.pdf"), height=6, width=6)
+    par(mar=c(10.1, 4.1, 4.1, 2.1))
+    boxplot(by.location, ylab=paste(names(genes)[x], "expression (TPM)"), las=2, col=rev(viridis::viridis(length(by.location))), main=genes[x])
+    dev.off()
+}
