6d0c960590e1f278020d4c1572af71cc0c1612c1
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-11-05 10:22:14 +0000

    Added script to cross-reference with E-MTAB-5308.

---
+++ rnaseq/submitAE/redundant.R
@@ -0,0 +1,8 @@
+already_there <- read.table("https://www.ebi.ac.uk/arrayexpress/files/E-MTAB-5308/E-MTAB-5308.sdrf.txt", header=TRUE, sep="\t", check.names=FALSE, stringsAsFactors=FALSE)
+current <- read.table("sdrf.tsv", header=TRUE, sep="\t", check.names=FALSE, stringsAsFactors=FALSE)
+
+m <- match(sub("p([12]).fq.gz", "_\\1.fq.gz", current[, "Array Data File"]), already_there[,"Comment[SUBMITTED_FILE_NAME]"])
+current$`Existing ENA run` <- already_there[,"Comment[ENA_RUN]"]
+current$`Existing FASTQ URI` <- already_there[,"Comment[FASTQ_URI]"]
+
+write.table(current, file="sdrf2.tsv", row.names=FALSE, col.names=TRUE, quote=FALSE, sep="\t")
