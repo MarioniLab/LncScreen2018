31014ca02cf7471469a5d4d4e19069b6594fa93c
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-11-19 13:17:52 +0000

    Checked alignment of full length lncRNA sequence.

--- chartseq/analysis/sequences/tppp_check.R
+++ chartseq/analysis/sequences/tppp_check.R
@@ -12,7 +12,7 @@ ref <- getSeq(BSgenome.Hsapiens.UCSC.hg38, chosen)
 names(ref) <- as.character(chosen)
 writeXStringSet(ref, file="TPPP.fa")
 
-# We also pull out the sequence of the lncRNA.
+# We also pull out the sequence of the lncRNA transcripts, plus the full-length (immature) sequence.
 
 library(EnsDb.Hsapiens.v86)
 all.tx <- select(EnsDb.Hsapiens.v86, keytype="GENEID", keys="ENSG00000231711", columns="TXID")
@@ -23,24 +23,31 @@ genome(trans) <- "hg38"
 transeq <- getSeq(BSgenome.Hsapiens.UCSC.hg38, trans)
 transeq <- lapply(transeq, paste, collapse="")
 transeq <- DNAStringSet(unlist(transeq))
-writeXStringSet(transeq, file="LINC00899.fa")
+
+full <- genes(EnsDb.Hsapiens.v86)["ENSG00000231711"]
+seqlevels(full) <- paste0("chr", seqlevels(full))
+genome(full) <- "hg38"
+fullseq <- getSeq(BSgenome.Hsapiens.UCSC.hg38, full)
+
+allseq <- c(transeq, fullseq)
+writeXStringSet(allseq, file="LINC00899.fa")
 
 # Comparing the best pairwise alignments with and without shuffling.
 
 pdf("alignments.pdf")
 for (x in names(ref)) {
-    for (y in names(transeq)) {
+    for (y in names(allseq)) {
         new.refs <- character(100)
         seq.src <- strsplit(as.character(ref[[x]]), "")[[1]]
         for (i in seq_along(new.refs)) {
             new.refs[i] <- paste(seq.src[sample(length(seq.src))], collapse="")
         }
-        aln.g.f <- pairwiseAlignment(DNAStringSet(new.refs), subject=transeq[y], type="local", scoreOnly=TRUE)
-        aln.g.r <- pairwiseAlignment(reverseComplement(DNAStringSet(new.refs)), subject=transeq[y], type="local", scoreOnly=TRUE)
+        aln.g.f <- pairwiseAlignment(DNAStringSet(new.refs), subject=allseq[y], type="local", scoreOnly=TRUE)
+        aln.g.r <- pairwiseAlignment(reverseComplement(DNAStringSet(new.refs)), subject=allseq[y], type="local", scoreOnly=TRUE)
         hist(pmax(aln.g.f, aln.g.r), xlab="Score", col="grey80", breaks=20, main=paste(x, "on", y))
 
-        aln.f <- pairwiseAlignment(ref[x], subject=transeq[y], type="local", scoreOnly=TRUE)
-        aln.r <- pairwiseAlignment(reverseComplement(ref[x]), subject=transeq[y], type="local", scoreOnly=TRUE)
+        aln.f <- pairwiseAlignment(ref[x], subject=allseq[y], type="local", scoreOnly=TRUE)
+        aln.r <- pairwiseAlignment(reverseComplement(ref[x]), subject=allseq[y], type="local", scoreOnly=TRUE)
         abline(v=pmax(aln.f, aln.r), col="red", lwd=2, lty=2)
     }
 }
